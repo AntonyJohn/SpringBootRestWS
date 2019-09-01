02d2992cd3deac955261e29f49a38230ac7c8f1b
Antony John <Antony John@DESKTOP-E31J38U>
2019-09-01 20:58:25 +0530

    code cleanup

--- src/main/java/com/antony/SpringBootRestWS/entrypoint/SpringSecurityConfig.java
+++ src/main/java/com/antony/SpringBootRestWS/entrypoint/SpringSecurityConfig.java
@@ -32,10 +32,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	
-    	/*http.requiresChannel()
-    	  .antMatchers().requiresSecure();*/
-    	
-        http
+    	http
             .logout()
             .and()
             // default with authentication
